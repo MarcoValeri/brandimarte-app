b9633c8ac448df0cd9b040c607cb82f63b4d0a32
MarcoValeri <marcovaleri@hotmail.it>
2026-04-30 17:00:44 +0100

    Create ContattiPage

---
+++ app/contatti/page.tsx
@@ -0,0 +1,17 @@
+import Carousel from "../components/Carousel/Carousel";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+
+const ContattiPage = () => {
+    return (
+        <div>
+            <Header subtitle="Contatti" />
+            <main>
+                <Carousel />
+                <Footer />
+            </main>
+        </div>
+    )
+}
+
+export default ContattiPage;
